debf1baae4078f491935d51f8b56a7c7221617ea
Henning Torsteinsen <henningnt@outlook.com>
2020-11-07 22:28:34 +0100

    Added more iteration. Added benchmarker.

--- Base64Kata/Base64Kata/Base64Konverter.cs
+++ Base64Kata/Base64Kata/Base64Konverter.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Linq;
 
 namespace Base64Kata
 {
     public class Base64Konverter
     {
-        private static readonly char[] lookup= new char[64]
+        private static readonly char[] lookup = new char[64]
           { 'A','B','C','D','E','F','G','H','I','J','K','L','M',
             'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
             'a','b','c','d','e','f','g','h','i','j','k','l','m',
@@ -33,8 +32,8 @@ namespace Base64Kata
 
                 result[resultIndex++] = lookup[(concated >> 18) & 0x3F];
                 result[resultIndex++] = lookup[(concated >> 12) & 0x3F];
-                result[resultIndex++] = lookup[(concated >>  6) & 0x3F];
-                result[resultIndex++] = lookup[ concated        & 0x3F];
+                result[resultIndex++] = lookup[(concated >> 6) & 0x3F];
+                result[resultIndex++] = lookup[concated & 0x3F];
 
                 remaining -= 3;
                 inputIndex += 3;
@@ -46,11 +45,11 @@ namespace Base64Kata
                 // Convert last group
                 var slice = span.Slice(inputIndex, 2);
                 var concated = slice[0] << 16 | slice[1] << 8;
-                
+
                 result[resultIndex++] = lookup[(concated >> 18) & 0x3F];
                 result[resultIndex++] = lookup[(concated >> 12) & 0x3F];
-                result[resultIndex++] = lookup[(concated >>  6) & 0x3F];
-                result[resultIndex]   = '=';
+                result[resultIndex++] = lookup[(concated >> 6) & 0x3F];
+                result[resultIndex] = '=';
             }
 
             if (remaining == 1)
@@ -63,7 +62,138 @@ namespace Base64Kata
                 result[resultIndex++] = lookup[(concated >> 18) & 0x3F];
                 result[resultIndex++] = lookup[(concated >> 12) & 0x3F];
                 result[resultIndex++] = '=';
-                result[resultIndex]   = '=';
+                result[resultIndex] = '=';
+            }
+
+            return new string(result);
+        }
+
+        public static string ToBase64v2(in string theString)
+        {
+            var span = theString.AsSpan();
+            var inputIndex = 0;
+            var resultIndex = 0;
+            var endIndex = span.Length;
+            // Create new array for result, adjust length for padding
+            var resultLength = span.Length;
+            if (span.Length % 3 == 1)
+            {
+                resultLength += 2;
+                endIndex -= 1;
+            }
+
+            if (span.Length % 3 == 2)
+            {
+                resultLength += 1;
+                endIndex -= 2;
+            }
+
+            var result = new char[resultLength * 8 / 6];
+
+            while (inputIndex < endIndex)
+            {
+                // Convert group
+                var slice = span.Slice(inputIndex, 3);
+                var concated = slice[0] << 16 | slice[1] << 8 | slice[2];
+
+                result[resultIndex++] = lookup[concated >> 18];
+                result[resultIndex++] = lookup[(concated >> 12) & 0x3F];
+                result[resultIndex++] = lookup[(concated >> 6) & 0x3F];
+                result[resultIndex++] = lookup[concated & 0x3F];
+
+                inputIndex += 3;
+            }
+
+            if (span.Length - endIndex == 2)
+            {
+                // Pad the last char
+                // Convert last group
+                var slice = span.Slice(inputIndex, 2);
+                var concated = slice[0] << 16 | slice[1] << 8;
+
+                result[resultIndex++] = lookup[concated >> 18];
+                result[resultIndex++] = lookup[(concated >> 12) & 0x3F];
+                result[resultIndex++] = lookup[(concated >> 6) & 0x3F];
+                result[resultIndex] = '=';
+            }
+
+            if (span.Length - endIndex == 1)
+            {
+                // Pad the last two chars
+                // Convert last group
+                var slice = span.Slice(inputIndex, 1);
+                var concated = slice[0] << 16;
+
+                result[resultIndex++] = lookup[concated >> 18];
+                result[resultIndex++] = lookup[(concated >> 12) & 0x3F];
+                result[resultIndex++] = '=';
+                result[resultIndex] = '=';
+            }
+
+            return new string(result);
+        }
+
+        public static string ToBase64v3(in string theString)
+        {
+            var span = theString.AsSpan();
+            var inputIndex = 0;
+            var resultIndex = 0;
+            var endIndex = span.Length;
+            // Create new array for result, adjust length for padding
+            var resultLength = span.Length;
+            if (span.Length % 3 == 1)
+            {
+                resultLength += 2;
+                endIndex -= 1;
+            }
+
+            if (span.Length % 3 == 2)
+            {
+                resultLength += 1;
+                endIndex -= 2;
+            }
+
+            var result = new char[resultLength * 8 / 6];
+
+            while (inputIndex < endIndex)
+            {
+                // Convert group
+                var slice = span.Slice(inputIndex, 3);
+                var concated = slice[0] << 16 | slice[1] << 8 | slice[2];
+
+                result[resultIndex    ] = lookup[concated >> 18];
+                result[resultIndex + 1] = lookup[(concated >> 12) & 0x3F];
+                result[resultIndex + 2] = lookup[(concated >> 6) & 0x3F];
+                result[resultIndex + 3] = lookup[concated & 0x3F];
+
+                inputIndex += 3;
+                resultIndex += 4;
+            }
+
+            if (span.Length - endIndex == 2)
+            {
+                // Pad the last char
+                // Convert last group
+                var slice = span.Slice(inputIndex, 2);
+                var concated = slice[0] << 16 | slice[1] << 8;
+
+                result[resultIndex++] = lookup[concated >> 18];
+                result[resultIndex++] = lookup[(concated >> 12) & 0x3F];
+                result[resultIndex++] = lookup[(concated >> 6) & 0x3F];
+                result[resultIndex] = '=';
+            }
+
+            if (span.Length - endIndex == 1)
+            {
+                // Pad the last two chars
+                // Convert last group
+                var slice = span.Slice(inputIndex, 1);
+                var concated = slice[0] << 16;
+
+                result[resultIndex++] = lookup[concated >> 18];
+                result[resultIndex++] = lookup[(concated >> 12) & 0x3F];
+                result[resultIndex++] = '=';
+                result[resultIndex] = '=';
             }
 
             return new string(result);
---
+++ Base64Kata/Benchmarker/Program.cs
@@ -0,0 +1,37 @@
+﻿using Base64Kata;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Benchmarker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+
+    public class Benchmarker
+    {
+        string testString = "øgjksøcjgsødfkljgsødlkfjgøsdlkfjgøsdlkfjgøsdlkfgjøsdlfkjgøsldkfgøsldkfjgølsdkfjgølsdkfjgølskdjfgølskdjfgølskdjfgøsldkfjgøsldkfjgøsldkjfgølskdfjgøgjksøcjgsødfkljgsødlkfjgøsdlkfjgøsdlkfjgøsdlkfgjøsdlfkjgøsldkfgøsldkfjgølsdkfjgølsdkfjgølskdjfgølskdjfgølskdjfgøsldkfjgøsldkfjgøsldkjfgølskdfjgøgjksøcjgsødfkljgsødlkfjgøsdlkfjgøsdlkfjgøsdlkfgjøsdlfkjgøsldkfgøsldkfjgølsdkfjgølsdkfjgølskdjfgølskdjfgølskdjfgøsldkfjgøsldkfjgøsldkjfgølskdfjgøgjksøcjgsødfkljgsødlkfjgøsdlkfjgøsdlkfjgøsdlkfgjøsdlfkjgøsldkfgøsldkfjgølsdkfjgølsdkfjgølskdjfgølskdjfgølskdjfgøsldkfjgøsldkfjgøsldkjfgølskdfjgøgjksøcjgsødfkljgsødlkfjgøsdlkfjgøsdlkfjgøsdlkfgjøsdlfkjgøsldkfgøsldkfjgølsdkfjgølsdkfjgølskdjfgølskdjfgølskdjfgøsldkfjgøsldkfjgøsldkjfgølskdfjgøgjksøcjgsødfkljgsødlkfjgøsdlkfjgøsdlkfjgøsdlkfgjøsdlfkjgøsldkfgøsldkfjgølsdkfjgølsdkfjgølskdjfgølskdjfgølskdjfgøsldkfjgøsldkfjgøsldkjfgølskdfjgøgjksøcjgsødfkljgsødlkfjgøsdlkfjgøsdlkfjgøsdlkfgjøsdlfkjgøsldkfgøsldkfjgølsdkfjgølsdkfjgølskdjfgølskdjfgølskdjfgøsldkfjgøsldkfjgøsldkjfgølskdfjg";
+
+        [Benchmark]
+        public void Testv1()
+        {
+            var actual = Base64Konverter.ToBase64v2(testString);
+        }
+
+        [Benchmark]
+        public void testv2()
+        {
+            var actual = Base64Konverter.ToBase64v2(testString);
+        }
+
+        [Benchmark]
+        public void testv3()
+        {
+            var actual = Base64Konverter.ToBase64v3(testString);
+        }
+    }
+}
